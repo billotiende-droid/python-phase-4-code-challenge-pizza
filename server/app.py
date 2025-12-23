59f2b01f38774592ebc8091b2debc92a9c9dc7e2
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-12-23 09:06:41 +0300

    add-GET-and-DELETE-routes

--- server/app.py
+++ server/app.py
@@ -24,6 +24,68 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+@app.route("/restaurants", methods=['GET'] )
+def get_restaurants():
+
+    restaurants = []
+
+    for restaurant in Restaurant.query.all():
+        # dictionar for JSON output
+        restaurant_dict ={
+            "address":restaurant.address,
+            "id":restaurant.id,
+            "name":restaurant.name
+        }
+        restaurants.append(restaurant_dict) 
+
+    return make_response(restaurants, 200)
+
+@app.route("/restaurants/<int:id>", methods =['GET'])
+def get_restaurant_by_id(id):
+
+    restaurant = Restaurant.query.filter(Restaurant.id ==id).first()
+    #error message for invalid ids
+    if not restaurant:
+        return make_response({"error":"Restaurant not available"}, 404)
+    
+    restaurant_dict ={
+        "address":restaurant.address,
+        "id":restaurant.id,
+        "name":restaurant.name,
+        "restaurant_pizzas":[]   #empty list which will be appended when using a for loop
+    }
+
+    for restaurant_pizza in restaurant.restaurant_pizzas:
+        restaurant_pizza_dict ={
+            "id":restaurant_pizza.id,
+            "pizza":{
+                "id":restaurant_pizza.id,
+                "name":restaurant_pizza.name,
+                "ingredients":restaurant_pizza.ingredients
+            },
+            "pizza_id":restaurant_pizza.pizza_id,
+            "price":restaurant_pizza.price,
+            "restaurant_id":restaurant_pizza.restaurant_id
+        }
+        #appends  the dictionary under the for loop to the empty list
+        restaurant_dict["restaurant_pizzas"].append(restaurant_pizza_dict)
+
+        return make_response(restaurant_dict, 200)
+    
+    @app.route("/restaurants/<int:id>", methods=['DELETE'])
+    def delete_restaurant_by_id(id):
+
+        restaurant =Restaurant.query.filter(Restaurant.id ==id).first()
+
+        if not restaurant:
+            return make_response({"error":"Restaurant not found"}, 404)
+
+        db.session.delete(restaurant)
+        db.session.commit()
+
+        return make_response("", 204) 
+     
+
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
